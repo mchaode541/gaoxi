467dcaa594ac4f725f047f24da85b33aa5ca80ac
mchaode541 <m13716720941@163.com>
2019-01-14 17:19:24 +0800

    测试项目搭建是否成功8

--- Gaoxi-Controller/src/main/java/com/gaoxi/controller/user/UserController.java
+++ Gaoxi-Controller/src/main/java/com/gaoxi/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package com.gaoxi.controller.user;
 
 import com.gaoxi.req.user.LoginReq;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-
+@RestController
 public interface UserController {
     /**
      * 登录
@@ -11,6 +13,6 @@ public interface UserController {
      * @param httpRsp HTTP响应
      * @return 登录是否成功
      */
-
+    @GetMapping("/login")
    public void login(LoginReq loginReq, HttpServletResponse httpRsp);
 }
