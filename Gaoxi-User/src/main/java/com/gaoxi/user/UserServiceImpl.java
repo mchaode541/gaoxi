5194db69fbdfd2708a6b9ba4058c25ad3cebf130
mchaode541 <m13716720941@163.com>
2019-01-15 15:43:11 +0800

    测试项目搭建是否成功12

--- Gaoxi-User/src/main/java/com/gaoxi/user/UserServiceImpl.java
+++ Gaoxi-User/src/main/java/com/gaoxi/user/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.gaoxi.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.gaoxi.dao.UserDao;
 import com.gaoxi.entity.user.UserEntity;
 import com.gaoxi.facade.user.UserService;
 import com.gaoxi.req.user.LoginReq;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * 描述:
@@ -14,6 +16,9 @@ import com.gaoxi.req.user.LoginReq;
 @Service(version = "1.0.0")
 @org.springframework.stereotype.Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public UserEntity login(LoginReq loginReq) {
