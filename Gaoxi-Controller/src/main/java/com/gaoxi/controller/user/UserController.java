6847ad73257bbb27f2f56145af3f2daa311f8c70
mchaode541 <m13716720941@163.com>
2019-01-16 21:31:07 +0800

    测试项目搭建是否成功23

--- Gaoxi-Controller/src/main/java/com/gaoxi/controller/user/UserController.java
+++ Gaoxi-Controller/src/main/java/com/gaoxi/controller/user/UserController.java
@@ -1,6 +1,5 @@
 package com.gaoxi.controller.user;
 
-import com.gaoxi.req.user.LoginReq;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
